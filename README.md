46b58da29b1b638fa8c22956d3de140ff7e46733
1051994 <41178717+1051994@users.noreply.github.com>
2018-07-21 19:56:53 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,111 +1,37 @@
-# Memory Game Project
+frontend-nanodegree-arcade-game
+===============================
 
-## Table of Contents
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
-* [Project Overview](#project-overview)
-* [How to play](#How-to-play)
-* [Project Specifications](#project-specifications)
-* [Acknowledgements](#acknowledgements)
-* [Contributing](#contributing)
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
 
-## Project Overview
 
-This (fend) project aims to :
-put my HTML, CSS, and JavaScript skills on a real project
-by building a browser-based card matching game.
+## Installing from Source
 
-### How The Game Works
+1. Clone the following repo: 
+   * git clone https://github.com/Ul1ra/ClassicArcadeGame
 
-The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match.
+2. Load index.html in browser.
 
-At each turn:
--   The player flips one card over to reveal its underlying symbol.
--   The player then turns over a second card, trying to find the corresponding card with the same symbol.
--   If the cards match, both cards stay flipped over.
--   If the cards do not match, both cards are flipped face down.
+3. Play the game
 
-The game ends once all cards have been correctly matched.
 
-And there is a leaderboard :
--   Represent high scores.
--   If you scored a new high score it highlights it and saves it.
+## How to Play:
+The goal is to match is to jump into the water. 
+Click in the following link to play: https://ul1ra.github.io/ClassicArcadeGame/
 
-### Screenshots
+1. Using the arrow keys on your keyboard, move upwards.
 
--   Game start screen
+2. Try and avoid the cockroaches and try and jump into the water.
 
-![Start Screen](screenshots/game-start.png "start screen")
+3. If you made it in the water, you can play again. If you collided with a cockroach, start again! 
 
--   Incorrect match action
 
-![Incorrect match](screenshots/incorrect-match.png "incorrect match")
+## Tools Used: 
 
--   Correct match action
+* Getting started Udacity excersize: 
+  * https://github.com/udacity/frontend-nanodegree-arcade-game
+  
 
-![Correct match](screenshots/correct-match.png "correct match")
-
--   Game win action
-
-![Win Screen](screenshots/game-win.png "game win")
-
--   High scores if you didn't get a high one.
-
-![High scores](screenshots/high-scores.png "High scores")
-
--   High scores if you successfully get a high one.
-
-![New high score](screenshots/new-high-score.png "New high score")
-
-### Game Functionality
-
-In the real-life game, players flip over cards to locate the pairs that match. The goal is to recreate this. These are the events that need to be handled:
--   Flipping cards
--   What happens when cards match
--   What happens when cards do not match
--   When the game finishes
-
-## How to play
-
-You can play the game from [here](https://magdi-mohammed.github.io/udacity-fend-project-memory-game/)
-or you can [clone](https://github.com/magdi-mohammed/udacity-fend-project-memory-game) it.
-
-## Project Specifications
-
-### Game Behavior
-
-| Criteria              | Specifications    |
-| --------------------- | ----------------- |
-| Memory Game Logic     | The game randomly shuffles the cards. A user wins once all cards have successfully been matched. |
-| Congratulations Popup | When a user wins the game, a modal appears to congratulate the player and ask if they want to play again. It should also tell the user how much time it took to win the game, and what the star rating was. |
-| Restart Button        | A restart button allows the player to reset the game board, the timer, and the star rating. |
-| Star Rating           | The game displays a star rating (from 1-3) that reflects the player's performance. At the beginning of a game, it should display 3 stars. After some number of moves, it should change to a 2 star rating. After a few more moves, it should change to a 1 star rating. |
-| Timer                 | When the player starts a game, a displayed timer should also start. Once the player wins the game, the timer stops. |
-| Move Counter          | Game displays the current number of moves a user has made. |
-
-### Interface Design
-
-| Criteria              | Specifications    |
-| --------------------- | ----------------- |
-| Styling               | Application uses CSS to style components for the game. |
-| Usability             | All application components are usable across modern desktop, tablet, and phone browsers. |
-
-### Documentation
-
-| Criteria              | Specifications    |
-| --------------------- | ----------------- |
-| README                | A `README` file is included detailing the game and all dependencies. |
-| Comments              | Comments are present and effectively explain longer code procedure when necessary. |
-| Code Quality          | Code is formatted with consistent, logical, and easy-to-read formatting as described in the [Udacity JavaScript Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html). |
-
-## Acknowledgements
-
--   [jQuery](https://jquery.com), The fast, small, and feature-rich JavaScript library which saved my time.
--   [Font Awesome](https://fontawesome.com/), for the vector icons
--   [Animate.css](https://github.com/daneden/animate.css), for my project cool, fun, and cross-browser animations.
--   ES06 thanks for the template literal and for-of loop ...
-
-## Contributing
-
-This repository is for a project for the Front-End Web Developer NanoDegree program at Udacity. Therefore, pull requests will not be accepted.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## Inspiration
+* Collision function: https://stackoverflow.com/questions/2440377/javascript-collision-detection
--- js/app.js
+++ js/app.js
@@ -1,302 +1,125 @@
-// ------------ Controller Area ----- //
-var Opencards = [];
-var Movcounter   = 0;
-// var stars      = 0;
-var ratingStars = $('i');
-var clickes   = 0;
-CardsList     = CardsImplement();
-var timer = document.querySelector(".timer");
-var interval;
-var gameEnded =  false;
-timer.innerHTML = "0 mins : 0 secs";
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
 
-var shuffled = shuffle(CardsList);
-DisplayCards();
-var click= 0;
+    // The following variables are used to determine the x and y axis and speed of the enemy
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
-//add event listner
-$('.card').on ('click',function(){
-	click++;
-	if(click == 1){ startTimer(); }
-	Matcher(this);
+    // The image of the enemy of cockroach that is added to the playing field 
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+
+    // Multiplies the speed by the dt parameter on the x axis
+    this.x += this.speed * dt;
+
+    // Once enemies are off the canvas, they reappear randomly with different speeds
+    if (this.x > 510) {
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 222);
+    };
+
+    // Checks for collisions between the player and the enemies
+    if (player.x < this.x + 80 &&
+        player.x + 80 > this.x &&
+        player.y < this.y + 60 &&
+        60 + player.y > this.y) {
+        player.x = 202;
+        player.y = 405;
+    };
+};
+
+// Renders the enemy into the game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Player class focusing on x and y axis
+var Player= function (x, y) {
+
+    // Variables for the player to move along x and y axis 
+    this.x = x;
+    this.y = y;
+
+    //The image of the player of horn-girl is added to the playing field 
+    this.Player= 'images/char-pink-girl.png';
+};
+
+Player.prototype.update = function (dt) {
+
+};
+
+// Renders the image of the user into the game
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.Player), this.x, this.y);
+};
+
+// Allows the user to use the arrow keys to jump from tile to tile
+Player.prototype.handleInput = function (keyPress) {
+
+    // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
+    if (keyPress == 'right' && this.x < 405) {
+        this.x += 102;
+    };
+
+    // Enables user on up arrow key to move upwards on the y axis by 83
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+
+    // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    };
+
+    // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position
+    if (this.y < 0) {
+        setTimeout(() => {
+            this.x = 202;
+            this.y = 405;
+        }, 800);
+    };
+};
+
+
+// All enemies are placed in an array
+var allEnemies = [];
+
+// Location of the 3 enemies on the y axis located on the stone road
+var enemyLocation = [63, 147, 230];
+
+
+// For each enemy located on the y axis from 0 on the x axis move at a speed of 200 
+// Until randomly regenerated in the enemy update function above
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
 });
 
-//----------------------- Model Area --------------------------//
-/* ---------------------Create a list to put all cards------------------ */ 
-function CardsImplement(){
-	var Cards=[];
-	Cards=document.getElementsByClassName("card");
-	return transformer(Cards);
-};
-// convert object into array 
-function transformer(obj){
-	var transformed =[];
-	for(var key in obj){
-		if(obj.hasOwnProperty(key)){
-			transformed.push(obj[key].innerHTML);
-		}
-	}
-	return transformed;
-}
- //----------------------------------------------------------------------------
-/* -------------------------------display cards --------------------------- */ 
-function DisplayCards(){
-	var list =CreateCards();
-	replacer (list);
-};
+// The starting location of the player is located at x=200, y=405
+var player = new Player(202, 405);
 
-function replacer(list){
-	document.getElementsByClassName("deck")[0].innerHTML= list.innerHTML;
-};
-//  -------------- end of display block -------------------
-// ------------ create cards to shuflled it--------------- 
-function CreateCards(){
-	var list = document.createElement("ul");
-	for(var x=0; x < shuffled.length ; x++){
-		var li = document.createElement("li");
-		li.innerHTML=shuffled[x];
-		li.classList.add("card");
-		list.appendChild(li);
-	}
-	return list;
-};
- // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
- // ---------------------- Done shuflled cards -------------------------
-//  -----------------------handel method ------------------
-function Matcher(card){
-	if(isClicked(card)){
-		return;
-	}
-	Displayicons(card);
-	Markedopen(card);
-}; // ------------------- Done handeld method ------------
-/*when cards is clicked this function will check cards thats already use show and open class or no 
-and return  true when using this clases */ 
- function isClicked(card)
- {
- 	if( $(card).hasClass("show") || $(card).hasClass("open")){
- 		return true;
- 	}
- 	return false;
- }//------------------------end of  checkd cards-------------------
-
- //  when i clicked cards this function 'Displayicons' will display icons on cards 
-function Displayicons(card){
-	$(card).addClass("show open");
-}; // ---------------------end of display icons -------------------
-
- // increment moves when user clicked cards 
-function incMoves(card){
-	if(gameEnded || $(card).hasClass("match") || $(card).is($(Opencards[0])) ){
-		return false;
-	}
-	Movcounter ++;
-	Rating(Movcounter);
-	$('.moves').text(Movcounter);
-};
-// -------------------------------end of increment moves----------------------
- /* Check  two cards is similar html content and class ' icons ' or no  */ 
-function IsMatch(Opencards){
-	let Case1= Opencards[0].innerHTML != Opencards[1].innerHTML; 
-	let Case2= $(Opencards[0]) .is($(Opencards[1]));
-	if(Case1 || Case2){
-		return false;
-	}else{
-		return true;
-	}
-};
-
-function matched(Opencards){
-	closeOpenCards(Opencards);
-	markAsMatched(Opencards);
-	openedCards = [];
-};
-
-function markAsMatched(Opencards){
-	for(let i= Opencards.length -1; i>= 0;i--)
-	{
-		$(Opencards[i]).addClass("match");
-	}
-}
-function nomatched(Opencards){
-	var currentcards = Opencards;
-	Animate(currentcards);
-	setTimeout(function(){
-		hideicons(currentcards);
-	},1000);
-	 openedCards = [];
-};
-// -----------------------------------end of matched ---------------------------------------------
-//-------------------------- rate to incremet moves when i clicked new card--------------
-function Rating(moves){
-  let score = 3;
-  if(moves <= 15) {
-    ratingStars.eq(3).removeClass('fa-star').addClass('fa-star-o');
-    score = 3;
-  } else if (moves > 15 && moves <= 22) {
-    ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
-    score = 2;
-  } else if (moves > 22) {
-    ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
-    score = 1;
-  }
-  return score;
-}
-//---------------------------- end of rating -----------------------
- //------------------------- animate cards-----------------------------
-function Animate(openedCards){
-	for (var i = openedCards.length - 1; i >= 0; i--) {
-		$(openedCards[i]).addClass("unmatched");
-	} 
-}
-// -----------------------End of the Animate-------------------
-//-------------------------start hide icons--------------
-function hideicons(Opencards){
-	for (var i = Opencards.length - 1; i >= 0; i--) {
-		$(Opencards[i]).removeClass("open show unmatched");
-	}
-}; // -----------------end of the hideicons---------------
-// -------------------- lock  open cards after matched it --------------
-function closeOpenCards(openedCards){
-	for (var i = openedCards.length - 1; i >= 0; i--) {
-		$(openedCards[i]).removeClass( "open" );
-	}
-}// ---------------------end of lock Open cards-----------------------
-  /* // ------------------------danger function ---------------------
- function styledanger(Opencards){
- 	for (var i = Opencards.length - 1; i >= 0; i--) {
- 		Opencards[i].classList.add("danger");
- 	}
- };
- // ---------------------end of the danger------------------- */ 
-
-function checkMatchedAll(){
-	var all = true;
-	$('.card').each(function(){
-		return all =  $(this).hasClass( "match"); // in just one not have it will fail
-	});// end each
-	if(all){
-		showStatistics();
-		gameEnded = true;
-	}
-}// end checkMatchedAll---------
-
-
-function handleReset (){
-	window.location.reload();
-};
-
-function showStatistics() {
-	var score = Rating(Movcounter);
-	clearInterval(interval);
-	var time  = getTimer();
-	owesomeAlert('Congratulation! Winning',  ' with ' + Movcounter + ' Moves '+ ' , Scoring ' + score + ' Star!' + '  in ' + time + ' Time ' , 'success', 'Play again', 'Stay');
-};
-// trying to implement es5 todd-------------------------
-function getTimer(){
-	return $('#timer').text();
-};
-
-function Markedopen(card){
-	if(Opencards.length > 0){
-		incMoves(card);
-		//Displayicons(card);
-		Opencards.push(card);
-		if(IsMatch(Opencards)){
-			matched(Opencards);
-			Opencards = [];
-		}else {
-			nomatched(Opencards);
-			Opencards = [];
-		}
-	}else {
-		Opencards.push(card);
-		incMoves(card);
-	}
-	checkMatchedAll();
-};
-
-
-// timer for calculate time gaming it
-var second = 0, minute = 0; hour = 0;
-function startTimer(){
-    interval = setInterval(function(){
-        timer.innerHTML = minute+" mins "+" : "+second+" secs";
-        second++;
-        if(second == 60){
-            minute++;
-            second=0;
-        }
-        if(minute == 60){
-            hour++;
-            minute = 0;
-        }
-    },1000);
-}
-// end of timer block 
-
-
-
-
-
-
-
-
-function owesomeAlert(titleToBind, textToShow ,typeToBind, confirmbtnText = null, cbtnText = null , confirmStyle = null){
-    swal({
-      title: titleToBind,
-      text: textToShow,
-      type: typeToBind,
-      showCancelButton: cbtnText ? true : false,
-      // showCancelButton: true, ATH [DONE flip dependency 2:40 pm 2018-01-17]
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: confirmbtnText,
-      cancelButtonText: cbtnText,
-      confirmButtonClass: 'btn btn-success',
-      cancelButtonClass: 'btn btn-danger',
-      buttonsStyling: false,
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        swal({
-          title: 'Reloading' ,
-          text:  'have fun todd.',
-          type : 'success',
-          timer : 2500
-        })//swal
-        setTimeout(function(){// submit from confirmOwesomeAlert has no time ATH
-            window.location.reload();
-        }, 2000);// setTimeOut
-      // result.dismiss can be 'cancel', 'overlay',
-      // 'close', and 'timer'
-      } else if (result.dismiss === 'cancel') {
-        swal({
-          title: 'Pausing',
-          text: 'Refresh the page to play again',
-          type: 'info',
-          animation: false,
-          customClass: 'animated tada',
-          timer: 1000
-        })//swal
-      }//fi
-    })
-
-}// function  owesomeAlert
-$(".restart").on("click",function(e)
-{
-	e.preventDefault();
-	handleReset();
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. 
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
 });
